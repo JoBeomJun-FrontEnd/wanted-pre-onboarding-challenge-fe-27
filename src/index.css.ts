511a2c8c38a020df748acf07efda0027e35fa0b0
JoBeomJun <oreasv3@gmail.com>
2024-10-04 22:05:57 +0900

    Update index.css.ts

--- src/index.css.ts
+++ src/index.css.ts
@@ -9,9 +9,12 @@ globalFontFace('PretendardVariable', {
   fontDisplay: 'swap',
 });
 
-globalStyle('*', {
-  fontFamily: Pretendard,
-});
+globalStyle(
+  'html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video',
+  {
+    fontFamily: Pretendard,
+  }
+);
 
 globalStyle('h1', {
   fontSize: '2.5em',
