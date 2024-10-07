911a47557e2310ff8893e2ac5fd29d12ef3a5f7e
JoBeomJun <oreasv3@gmail.com>
2024-10-08 01:36:20 +0900

    Update index.css.ts

--- src/index.css.ts
+++ src/index.css.ts
@@ -15,17 +15,3 @@ globalStyle(
     fontFamily: Pretendard,
   }
 );
-
-globalStyle('h1', {
-  fontSize: '2.5em',
-  marginBottom: '1em',
-});
-
-globalStyle('a', {
-  color: '#0070f3', // 링크 색상
-  textDecoration: 'none', // 밑줄 제거
-});
-
-globalStyle('a:hover', {
-  textDecoration: 'underline', // 마우스 오버 시 밑줄 추가
-});
